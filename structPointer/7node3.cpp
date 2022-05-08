ccafda04fdf8d28ff6b58051d7e79c37ff58402c
faizan <faizannazir289@gmail.com>
2022-05-08 13:04:51 +0500

    gdb debug installed

---
+++ structPointer/7node3.cpp
@@ -0,0 +1,123 @@
+/** 
+ * Node lecture 2 
+ *  Create a function (Take current node and value as input ) which will: 
+ * 1. create a node                     
+ *    node *n;    n=new node;
+ * 2. assign it a value                 
+ *    n->val = val; n->next = NULL
+ * 3. link with current node            
+ *    current-> next = n;
+ * 4. return node address               
+ *    return n;
+ * */
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ 
+// Solving Length problem in struct to make it like list
+// node will contain its own value and a pointer
+// node is like a list but of same data type
+struct node
+{
+    int val;
+    node *next;  // Pointer will point next node 
+
+};
+node* currentNode(node *current,int val);
+node* headNode(int val);
+void print(node *current);
+void delVal(node *current);
+
+int main()
+{
+    node *head , *current, *tail;      // Pointer of type node 
+    head = NULL;      // initialized a pointer with NULL value
+    // head = new node;   // create a node and return its address
+    // head->val = 0;
+    // head->next = NULL;  // Initialize Node pointer with NULL
+    current = head;     // current pointing head node
+    tail = head;
+cout<<"**************** Creating List ***************************"<<endl;
+
+for(int i=0; i<=10; i++)
+{    
+     if(head==NULL)
+     {
+         head =headNode(i);
+         current = head;
+     }
+     else
+     {
+         current = currentNode(current,i);
+         tail = current;
+  
+     }
+
+}
+cout<<"**************** List Created  ***************************"<<endl;
+current=head;
+print(head);
+while (current->next->val!=5)
+{
+    current = current->next;
+}
+// current=current->next;
+cout<<"Deleting node with value =  "<<current->next->val<< endl;
+delVal(current);
+cout<<" After Deleting node "<< endl;
+print(head);
+
+}
+
+
+
+/**
+ * @brief 
+ *  creating node 
+ * @param current 
+ * @param val 
+ * @return node* 
+ * 1. create a new node 
+ * 2. assign new node(n) passed val
+ * 3. point next of new node to NULL
+ * 4. assign current node  pointer to new node(n)
+ */
+node* currentNode(node *current,int val)
+{
+    node *n;    
+    n=new node;
+    n->val =val;
+    n->next = NULL;
+    current-> next = n;
+    return n;
+}
+node* headNode(int val)
+{
+    node *n;    
+    n=new node;
+    n->val =val;
+    n->next = NULL;
+    return n;
+}
+void print(node *current)
+{
+cout<<"**************** Printing Node ***************************"<<endl;
+cout<<"[";
+while (current->next!=NULL)
+{
+    cout<<"value = "<< current->val<<",   ";
+    cout<<"next = "<< current->next<<",   "<<endl;
+    current = current->next;
+
+}
+cout<<"value = "<<current->val<<"     next = "  << current->next <<endl;
+cout<<"]";
+}
+void delVal(node * current)
+{
+    node *temp;
+    temp = current->next;
+    current->next = current->next->next;
+    delete temp;
+    temp = NULL;
+}
